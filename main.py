ff494d39ae399f9bf43287d267318a3b58b65dda
diegao <diegonoguerarec@gmail.com>
2026-08-23 16:23:30 -0300

    Usar solo canal verde

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from difusion import *
 if __name__ == '__main__':
     imagen = leer_imagen_ppm_rgb(ruta_imagen='entrada/im0001.ppm')
 
+    # Usar solo canal verde
+    imagen = imagen[:, :, 1:2]
+
     imagen_difundida = difusion_anisotropica_rgb(imagen=imagen)
 
     guardar_imagen_rgb(imagen_rgb=imagen, ruta_salida='salida/im0001.png')
